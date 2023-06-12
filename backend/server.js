b04ce9983d8ffd73d3ed17e476ee7e9c28239ed0
Jayakanth <jayakanth1812@gmail.com>
2023-06-12 14:53:46 +0530

    backend db connection

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+const app = exprss();
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'login'
+})
+
+app.listen(5173, () => console.log('server started'))
